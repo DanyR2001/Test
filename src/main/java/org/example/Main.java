85e49b4462ef3fe09b3578e2e8ad5e2bd03f4af0
DanyR2001 <danielerusso_@hotmail.it>
2022-09-30 12:50:21 +0200

    aggiunta print2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("sciao bello");
         System.out.println("sciao bello1");
+        System.out.println("sciao bello2");
 
     }
 }
